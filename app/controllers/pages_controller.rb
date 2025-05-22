58e0e2e83bd3c6a56ddfce36cef70abd21867916
Diana Culita <diana.culita100@gmail.com>
2025-05-22 17:26:12 +0100

    Cloudinary images added to carousel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    
+    @galleries = Gallery.order(created_at: :desc).limit(5)
   end
 end
