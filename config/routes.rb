4ac4f8ab4dfdf21d48374821e42753e6dd75266d
Diana Culita <diana.culita100@gmail.com>
2025-05-22 18:41:44 +0100

    Home page view cloudinary images added artists cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get "galleries/index"
-  get "galleries/show"
-  get "galleries/create"
   devise_for :users
   root to: "pages#home"
+  resources :galleries, only: [:index, :show, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
